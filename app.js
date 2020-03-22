1f0247f8392ebef05c6ef5d4b003a1d2be6d2d6e
Alfafis <ctpmmg2009_101@hotmail.com>
2020-03-22 00:47:53 -0300

    boticario

---
+++ app.js
@@ -0,0 +1,49 @@
+window.onload = function () {
+   vm.getJson();
+   forJson();
+}
+
+const vm = new Vue({
+   el: '#app',
+   data: {
+      actions: {},
+   },
+   methods: {
+      getJson() {
+         fetch('./boticario.json')
+            .then(response => response.json())
+            .then(json => {
+               this.actions = json;
+               console.log(this.actions);
+            })
+      }
+   }
+});
+
+let click = 0;
+let precoTotal = 0;
+let iconMarket = document.querySelector(".float > i");
+let carrinho = document.querySelector(".totalMarket");
+
+const clickONCard = (e) => {
+   let clickCard = document.querySelector(".card > .value-product > span").innerText;
+   click++;
+   precoTotal = clickCard * click;
+   iconMarket.classList.remove("fa-shopping-cart");
+   iconMarket.classList.add("fa-trash");
+   showValue();
+}
+
+const clearMarket = () => {
+   precoTotal = 0;
+   click = 0;
+   console.log(precoTotal);
+   iconMarket.classList.remove("fa-trash");
+   iconMarket.classList.add("fa-shopping-cart");
+   carrinho.innerHTML = "";
+}
+
+const showValue = () => {
+   const toTalValue = 0;
+   carrinho.innerHTML = "<span>" + "R$ " + precoTotal + "</span>";
+}
